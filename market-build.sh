4d02bfee30836b6fdc9e32a129dd875734059f50
jde-cpp <johnduffyx@outlook.com>
2021-05-24 04:39:19 -0400

    windows build.

--- market-build.sh
+++ market-build.sh
@@ -1,17 +1,15 @@
+#!/bin/bash
 clean=${1:-0};
 shouldFetch=${2:-1};
 buildFramework=${3:-1};
-source source-build.sh;
+#source source-build.sh;
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+stage=$REPO_DIR/jde/Public/stage
 echo market-build.sh clean=$clean shouldFetch=$shouldFetch buildFramework=$buildFramework
-
+if [[ -z $sourceBuild ]]; then source $scriptDir/../Framework/source-build.sh; fi;
 if [ $buildFramework -eq 1 ]; then
  	$scriptDir/../Framework/framework-build.sh $clean $shouldFetch; if [ $? -ne 0 ]; then echo framework-build.sh failed - $?; exit 1; fi;
 else
-	#echo market-build says sourceBuild=\'$sourceBuild\';
-	if [[ -z $sourceBuild ]]; then source source-build.sh; fi;
-	toBashDir $REPO_DIR REPO_BASH
 	findExecutable protoc.exe $REPO_BASH/protobuf/cmake/build/sln/Release;
 fi;
 
@@ -35,27 +33,28 @@ function xzBuildLib
 		fi;
 	else
 		echo Not found:  $dir.
+		exit 1;
 	fi;
 	cd $previousDir;
 }
 xzBuildLib
-echo `pwd`;
+cd $REPO_DIR/jde;
 fetchBuild XZ 0
 
 function protocBuild()
 {
-	publicDir=$2;
+	publicDir=${2:-0};
 	if [ ! -f $1.pb.cc ]; then
 		if [ $publicDir -eq 1 ]; then
 			prevDir=`pwd`;
 			workDir=$baseDir/$jdeRoot/Public/jde/markets/types/proto;
 			cd $workDir;
 		fi;
-		cmd="protoc --cpp_out dllexport_decl=JDE_MARKETS_EXPORT:. $1.proto";
-		$cmd;
+		cmd2="protoc --cpp_out dllexport_decl=JDE_MARKETS_EXPORT:. $1.proto";
+		$cmd2;
 		if [ $? -ne 0 ]; then
 			echo `pwd`;
-			echo $cmd;
+			echo $cmd2;
 			exit 1;
 		fi;
 		if [ $publicDir -eq 1 ]; then
@@ -67,17 +66,22 @@ function protocBuild()
 
 function marketLibraryProtoc
 {
+	findExecutable protoc.exe $stage/release;
+	echo marketLibraryProtoc;
 	cd types/proto;
 	if [ $clean -eq 1 ]; then
 		rm *.pb.cc
 		rm *.pb.h
 	fi;
-	protocBuild watch;
-	protocBuild requests;
-	protocBuild results;
-	#sed -i 's/class Fundamentals_ValuesEntry_DoNotUse/class JDE_MARKETS_EXPORT Fundamentals_ValuesEntry_DoNotUse/' results.pb.h
-	#sed -i 's/class StringMap_ValuesEntry_DoNotUse/class JDE_MARKETS_EXPORT StringMap_ValuesEntry_DoNotUse/' results.pb.h
-	protocBuild ib;
+	protocBuild watch 1;
+	protocBuild requests 1;
+	protocBuild results 1;
+	pushd `pwd` > /dev/null;
+	cd $baseDir/$jdeRoot/Public/jde/markets/types/proto;
+	sed -i 's/class Fundamentals_ValuesEntry_DoNotUse/class JDE_MARKETS_EXPORT Fundamentals_ValuesEntry_DoNotUse/' results.pb.h;
+	sed -i 's/class StringMap_ValuesEntry_DoNotUse/class JDE_MARKETS_EXPORT StringMap_ValuesEntry_DoNotUse/' results.pb.h;
+	popd;
+	protocBuild ib 1;
 	protocBuild OptionOI;
 	protocBuild bar;
 	protocBuild edgar 1;
@@ -90,28 +94,23 @@ function marketLibraryProtoc
 			cd "$twsDir"
 			mklink TwsSocketClient64.vcxproj $sourceDir
 			cd $sourceDir;
-		#else
-			#echo Found \"$twsDir/TwsSocketClient64.vcxproj\";
 		fi;
-	 	prevDir=`pwd`;
-		echo cd "$twsDir";
-	 	cd "$twsDir";
-		#echo buildWindows TwsSocketClient64;
-	 	buildWindows TwsSocketClient64 TwsSocketClient64.dll;
-	 	cd $prevDir;
-		moveToDir .bin;
-		moveToDir debug;
-		#echo pwd=`pwd`;
-		#echo mklink TwsSocketClient.lib "$twsDir/.bin/debug";
-		mklink TwsSocketClient64.lib "$twsDir/.bin/debug";
-		cd ..;
-		moveToDir release;
-		mklink TwsSocketClient64.lib "$twsDir/.bin/release";
-		cd ../..;
+		if [ ! -f  $stage/release/TwsSocketClient64.dll ]; then
+			prevDir=`pwd`;
+			cd "$twsDir";
+			buildWindows TwsSocketClient64 TwsSocketClient64.dll;
+			cd $stage/debug;
+			mklink TwsSocketClient64.lib "$twsDir/.bin/debug"; mklink TwsSocketClient64.dll "$twsDir/.bin/debug";
+			cd $stage/release;
+			mklink TwsSocketClient64.lib "$twsDir/.bin/release"; mklink TwsSocketClient64.dll "$twsDir/.bin/release";
+			cd $prevDir;
+		fi;
 	else
 		echo Not Found:  \"c:\\TWS API\"
 	fi;
 }
-fetchDefault MarketLibrary 0; 
+fetchDefault MarketLibrary 0;
+echo build fetchDefault MarketLibrary 0;
 marketLibraryProtoc;
-build MarketLibrary 0 Jde.Markets.dll;
+echo build MarketLibrary;
+build MarketLibrary 0 Jde.Markets.dll $clean;
